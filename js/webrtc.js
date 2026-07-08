c42dd9e4a390f3ff4fe30106e90b5c981598d20f
dahiana135798-star <dahiana135798@gmail.com>
2026-07-07 21:20:05 -0300

    Implement WebRTC call features with remote stream
    Added remote stream handling and call initiation functionality.

--- js/webrtc.js
+++ js/webrtc.js
@@ -1,26 +1,30 @@
 // Variables
 
 let localStream;
+let remoteStream;
+let peerConnection;
+
+const servers ={
+	iceServers: [
+	{ urls: "stun:stun.l.google.com:19302" }
+	]
+};
 
 //Elementos
 
 const btnCamara =
-document.getElementById(
-"btnCamara"
-);
+document.getElementById("btnCamara");
 
-const btnMicrofono =
-document.getElementById(
-"btnMicrofono"
-);
+const btnMicrofono = document.getElementById("btnMicrofono");
 
-const videoLocal =
-document.getElementById(
-"videoLocal"
-);
+const btnLlamada = document.getElementById("btnLlamada");
+
+
+const videoLocal = document.getElementById("videoLocal");
+
+const videoRemoto = document.getElementById("videoRemoto");
 
 //Activar camara
-
 if(btnCamara){
 	
 btnCamara.addEventListener(
@@ -45,6 +49,7 @@ async function activarCamara(){
 		
 		videoLocal.srcObject =
 		localStream;
+		alert("Camara activada");
 		
 	}
 	
@@ -83,15 +88,39 @@ function toggleMicrofono(){
 		
 	}
 	
-	const audioTracks =
-	localStream.getAudioTracks();
-	
-	audioTracks.forEach(track=>{
-		
+	localStream.getAudioTracks().forEach(track=>{
 		track.enabled =
 		!track.enabled;
 		
 	});
 	
 }
-console.log("webrtc.js cargado");
+
+//Iniciar llamada
+if(btnLlamada){
+	btnLlamada.addEventListener("click"), iniciarLlamada);
+}
+
+async function iniciarLlamada() {
+	if(!localStream){
+		alert("Primero activa la camara");
+		return;
+	}
+	peerConnection = new RTCPeerConnection(servers);
+	
+	//Agregar pistas locales
+	localStream.getTrack/track, localStream);
+});
+
+//Mostrar stream remoto
+peerConection.ontrack=event => {
+	videoRemoto.srcObject = event.streams[0];
+};
+
+//Crear oferta
+const offer = await peerConnection.createOffer();
+await peerConnection.setLocalDescription(offer);
+
+//enviar oferta
+console.log("oferta creada:", offer);
+}
